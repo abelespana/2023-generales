f06dc13b06271e025a45f95a3d865f1a1748f81e
Abel España (MBP) <abel.espana@me.com>
2023-07-16 21:40:49 +0200

    (feat) fetching results

--- src/pages/results-page.jsx
+++ src/pages/results-page.jsx
@@ -1,4 +1,25 @@
+import { useEffect } from 'react'
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebaseConfig";
+
 const ResultsPage = () => {
+	const fetchResults = async () => {
+		const results = [];
+		const querySnapshot = await getDocs(collection(db, 'generales'));
+
+		querySnapshot.forEach((doc) => {
+			const data = doc.data();
+			results.push(data.results);
+		});
+
+		return results;
+	}
+
+	useEffect(() => {
+		fetchResults();
+	}, [])
+
+
 	return (
 		<h1>Results Page works!</h1>
 	)
